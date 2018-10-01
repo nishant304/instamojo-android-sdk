3fce15c6ef3f2099483bf49a1bbaa5ecb654ab97
Sai Prasad <saiprasad.ch@gmail.com>
2018-10-01 14:34:59 +0530

    Add appropriate `NotNull` annotation in `EMIBankList` view.

--- android-sdk/src/main/java/com/instamojo/android/fragments/EMIBankList.java
+++ android-sdk/src/main/java/com/instamojo/android/fragments/EMIBankList.java
@@ -1,6 +1,7 @@
 package com.instamojo.android.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +24,7 @@ public class EMIBankList extends BaseFragment {
 
     @SuppressWarnings("unchecked")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_emi_instamojo, container, false);
         parentActivity = (PaymentDetailsActivity) getActivity();
